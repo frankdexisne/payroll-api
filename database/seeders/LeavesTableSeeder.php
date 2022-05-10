db1262224a36cbfe698af1581cd914148127c8bd
Frankly D. Dexisne Jr <knarfensixed@gmail.com>
2022-05-10 10:27:11 +0800

    Leave table seeders

--- database/seeders/LeavesTableSeeder.php
+++ database/seeders/LeavesTableSeeder.php
@@ -18,27 +18,27 @@ class LeavesTableSeeder extends Seeder
             [
                 'name' => 'Vacation Leave',
                 'no_of_days' => 0,
-                'for_male' => null
+                'gender_id' => null
             ],
             [
                 'name' => 'Paternity Leave',
                 'no_of_days' => 0,
-                'for_male' => 1
+                'gender_id' => 1
             ],
             [
                 'name' => 'Maternity Leave',
                 'no_of_days' => 0,
-                'for_male' => 2
+                'gender_id' => 2
             ],
             [
                 'name' => 'Sick Leave',
                 'no_of_days' => 0,
-                'for_male' => null
+                'gender_id' => null
             ],
             [
                 'name' => 'Unpaid Leave',
                 'no_of_days' => 0,
-                'for_male' => null
+                'gender_id' => null
             ],
         ];
         Leave::upsert($data, ['name'], ['name']);
